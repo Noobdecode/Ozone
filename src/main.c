652afaa8a3453b8f81d5ff76239176b7d76b0e1c
Jayed Siddiq <jayed.pcc@gmail.com>
2026-07-25 18:46:19 +0600

    Adding the startShell function within main.c

--- src/main.c
+++ src/main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
-void showWelcomeScreen(void)
-{
-    printf("==========================\n");
-    printf("     Welcome to Ozone     \n");
-    printf("       Version 0.1\n");
-    printf("==========================\n");
-}
+void showWelcomeScreen(void);
+void startShell(void);
+
 int main(void)
 {
     showWelcomeScreen();
 
+    startShell();
+
     return 0;
+}
+void showWelcomeScreen(void)
+{
+    printf("=================================\n");
+    printf("        Welcome to Ozone\n");
+    printf("          Version 0.1\n");
+    printf("=================================\n");
+}
+void startShell(void)
+{
+    printf("\nStarting Ozone Shell...\n");
 }
