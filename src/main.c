e6d93f6ccf0ad3c99e62697413d2c31fa3abc198
Jayed Siddiq <jayed.pcc@gmail.com>
2026-07-31 22:39:48 +0600

    Implement first working shell command (help)

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 
 void showWelcomeScreen(void);
 void startShell(void);
+void help(void);
 
 int main(void)
 {
@@ -23,11 +24,33 @@ void startShell(void)
 {
     char command[100];
 
+    char commands[5][100] =
+    {
+        "help",
+        "exit",
+        "version",
+        "about",
+        "clear"
+    };
+
     printf("\nStarting Ozone Shell...\n");
     while (1)
     {
     printf("\nguest@ozone:/home$ ");
     scanf("%99s", command);
+    for (int i = 0; i < 5; i++)
+    {
+            if (strcmp(command, commands[i]) == 0)
+            {
+                switch (i)
+                {
+                    case 0:
+                    help();
+                    break;
+                }
+            }
+
+    }
     if (strcmp(command,"exit") == 0)
     {
         printf("shutting down Ozone...\n");
@@ -37,4 +60,21 @@ void startShell(void)
 
     }
 
+}
+void help(void)
+{
+ printf("====================================\n");
+    printf("          OZONE HELP\n");
+    printf("====================================\n\n");
+
+    printf("Welcome to Ozone!\n");
+    printf("Type one of the commands below.\n\n");
+
+    printf("help      - Display this help menu.\n");
+    printf("about     - Show information about Ozone.\n");
+    printf("version   - Display the current Ozone version.\n");
+    printf("clear     - Clear the screen.\n");
+    printf("exit      - Shut down Ozone.\n");
+
+    printf("\n====================================\n");
 }
